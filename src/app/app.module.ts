662974a30740cf151d5580b7d5abc700b8d86539
ConxiCr <conxicr@hotmail.com>
2021-03-23 08:01:28 +0100

    create component "ListaProductos" and "Comanda"
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+import { ComandaComponent } from './components/comanda/comanda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductosComponent,
+    ComandaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/comanda/comanda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comanda',
+  templateUrl: './comanda.component.html',
+  styleUrls: ['./comanda.component.css']
+})
+export class ComandaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
